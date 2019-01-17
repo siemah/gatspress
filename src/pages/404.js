ade4aaf8e313c96c22fa4054c1e458a2de941baa
siemah <28986331+siemah@users.noreply.github.com>
2019-01-17 13:33:55 +0100

    window bug

--- src/pages/404.js
+++ src/pages/404.js
@@ -3,12 +3,21 @@ import React from 'react'
 import Layout from '../components/layout'
 import SEO from '../components/seo'
 
-const NotFoundPage = () => (
-  <Layout>
-    <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </Layout>
-)
+class NotFoundPage extends React.Component {
+  _onLoad = e => console.log(window)
+  componentWillMount = () => {
+    console.log(window)
+    this._onLoad();
+  }
+  render () {
+    return (
+    <Layout>
+      <SEO title="404: Not found" />
+      <h1>NOT FOUND</h1>
+      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    </Layout>
+  )
+  }
+}
 
 export default NotFoundPage
